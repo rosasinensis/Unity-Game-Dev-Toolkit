48a18536d324bc7db991364398957a90180a9b6d
rosasinensis <kclemente@gmail.com>
2026-01-03 01:34:33 -0800

    Adds (simple) StateMachine

---
+++ StateMachine/StateController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace MKUtil.SM
+{
+
+    // T is some IStateMachine which takes some IState
+
+    public class StateController<T, V> where T : IStateMachine<V>
+    {
+
+        // Something which holds a state machine and its states (by id).
+        // NOTE: Must be executed manually by calling Execute().
+
+        private Dictionary<string, V> _states = new Dictionary<string, V>();
+        private T _stateMachine;
+
+        public void Initialize(T stateMachine)
+        {
+            this._stateMachine = stateMachine;
+        }
+        public void Execute()
+        {
+            _stateMachine?.Execute();
+        }
+        public void AddState(string id, V state)
+        {
+            if (_states.ContainsKey(id))
+            {
+                DebugLogger.Log(GetType(), $"State '{id}' already exists. Overwriting.");
+                _states[id] = state;
+            }
+            else
+            {
+                _states.Add(id, state);
+            }
+        }
+        public void ChangeState(string id)
+        {
+            if (TryGet(id, out V state))
+            {
+                _stateMachine.ChangeState(state);
+            }
+            else
+            {
+                DebugLogger.Log(GetType(), $"No id {id} exists in state machine.");
+            }
+        }
+        public void RemoveState(string id)
+        {
+            if (_states.ContainsKey(id))
+            {
+                _states.Remove(id);
+            }
+        }
+        public bool TryGet(string id, out V result)
+        {
+            return (_states.TryGetValue(id, out result));
+        }
+
+    }
+
+    // EXAMPLE
+
+    //public class WeatherManager : MonoBehaviour
+    //{
+    //    // You define the specific types here
+    //    private StateController<StateMachine<WeatherState>, WeatherState> _weatherController;
+
+    //    void Awake()
+    //    {
+    //        // Controller
+    //        _weatherController = new StateController<StateMachine<WeatherState>, WeatherState>();
+
+    //        // Initialize
+    //        var machine = new StateMachine<WeatherState>();
+    //        _weatherController.Initialize(machine);
+
+    //        // Add States.
+    //        _weatherController.AddState("Sunny", new SunnyState());
+    //        _weatherController.AddState("Rainy", new RainyState());
+    //    }
+
+    //    void Update()
+    //    {
+    //        _weatherController.Execute();
+    //    }
+    //}
+
+}
---
+++ StateMachine/StateMachine.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MKUtil.SM
+{
+    [SerializeField]
+    public abstract class IState
+    {
+        // States. Implement it for use with StateMachine<T>.
+
+        internal virtual void Enter() { }
+        internal virtual void Execute() { }
+        internal virtual void Exit() { }
+    }
+
+    public interface IStateMachine<T>
+    {
+        // Doesn't necessarily take IState.
+
+        T CurrentState { get; set; }
+        void Execute();
+        void ChangeState(T newState);
+    }
+
+    public class StateMachine<T> : IStateMachine<T> where T : IState
+    {
+
+        // Simple state machine. Takes something that implements IState.
+        // Call Execute() to run it.
+
+        public T CurrentState { get; set; }
+
+        public void Execute()
+        {
+            if (CurrentState != null)
+            {
+                OnExecute();
+            }
+        }
+        public virtual void ChangeState(T newState)
+        {
+            if (CurrentState != null)
+            {
+                OnExit();
+            }
+            CurrentState = newState;
+            OnEnter();
+        }
+
+        public virtual void OnExecute()
+        {
+            CurrentState.Execute();
+        }
+        public virtual void OnExit()
+        {
+            CurrentState.Exit();
+        }
+        public virtual void OnEnter()
+        {
+            CurrentState.Enter();
+        }
+
+    }
+
+}
